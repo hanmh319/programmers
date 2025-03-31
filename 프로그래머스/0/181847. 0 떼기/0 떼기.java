a51e103d1e5c41963efe37f39f9f6e9add4876b1
hanmh319 <98718771+hanmh319@users.noreply.github.com>
2025-03-31 17:01:28 +0900

    [level 0] Title: 0 떼기, Time: 9.63 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int forAnswer = Integer.valueOf(n_str);
+        answer = forAnswer + "";
+        return answer;
+    }
+}
